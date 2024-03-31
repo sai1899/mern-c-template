6cf58c3bd1b6466b85002bff65b249610b1bc02d
sai1899 <saicsk579@gmail.com>
2024-03-31 11:51:03 +0530

    test lint staged

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,32 +1,24 @@
 module.exports = {
-  'env': {
-    'browser': true,
-    'commonjs': true,
-    'es2021': true,
-  },
-  'extends': [
-    'google',
-    'plugin:react/recommended',
-  ],
-  'overrides': [
-    {
-      'env': {
-        'node': true,
-      },
-      'files': [
-        '.eslintrc.{js,cjs}',
-      ],
-      'parserOptions': {
-        'sourceType': 'script',
-      },
+    env: {
+        browser: true,
+        commonjs: true,
+        es2021: true,
     },
-  ],
-  'parserOptions': {
-    'ecmaVersion': 'latest',
-  },
-  'plugins': [
-    'react',
-  ],
-  'rules': {
-  },
+    extends: ["google", "plugin:react/recommended"],
+    overrides: [
+        {
+            env: {
+                node: true,
+            },
+            files: [".eslintrc.{js,cjs}"],
+            parserOptions: {
+                sourceType: "script",
+            },
+        },
+    ],
+    parserOptions: {
+        ecmaVersion: "latest",
+    },
+    plugins: ["react"],
+    rules: {},
 };
--- src/server.js
+++ src/server.js
@@ -1,3 +1,3 @@
-const a = [1, 2, 4];
+const a = [1, 2, 3, 5];
 
 console.log(a);
